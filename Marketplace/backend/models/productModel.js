c7d74bf3fcb559f5ab8234766970083c84e2cfbe
Mineth Sithmaka <115918744+MinoShaz99@users.noreply.github.com>
2024-04-20 01:14:19 +0530

    Add Sidebar & SearchBox

--- Marketplace/backend/models/productModel.js
+++ Marketplace/backend/models/productModel.js
@@ -6,7 +6,7 @@ const productSchema = new mongoose.Schema(
     slug: { type: String, required: true },
     category: { type: String, required: true },
     image: { type: String, required: true },
-    Artist: { type: String, required: true, unique: true },
+    Artist: { type: String, required: true },
     price: { type: Number, required: true },
     countInStock: { type: Number, required: true },
     Size: { type: String, required: true },
