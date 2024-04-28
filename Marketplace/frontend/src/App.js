d37cc01f8970f36fe250736407c9cc1b70ac3e7a
Mineth Sithmaka <115918744+MinoShaz99@users.noreply.github.com>
2024-04-28 09:18:48 +0530

    List Users

--- Marketplace/frontend/src/App.js
+++ Marketplace/frontend/src/App.js
@@ -35,6 +35,7 @@ import DashboardScreen from './screens/DashboardScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
+import UserListScreen from './screens/UserListScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -266,6 +267,14 @@ function App() {
                 element={
                   <AdminRoute>
                     <OrderListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/users"
+                element={
+                  <AdminRoute>
+                    <UserListScreen />
                   </AdminRoute>
                 }
               ></Route>
