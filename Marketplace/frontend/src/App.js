f5d483fd2427fdb03a2ce9981b240306975478aa
Mineth Sithmaka <115918744+MinoShaz99@users.noreply.github.com>
2024-04-28 09:38:15 +0530

    Edit Users Screen

--- Marketplace/frontend/src/App.js
+++ Marketplace/frontend/src/App.js
@@ -36,6 +36,7 @@ import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
 import OrderListScreen from './screens/OrderListScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -291,6 +292,14 @@ function App() {
                 element={
                   <AdminRoute>
                     <ProductEditScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+              <Route
+                path="/admin/user/:id"
+                element={
+                  <AdminRoute>
+                    <UserEditScreen />
                   </AdminRoute>
                 }
               ></Route>
